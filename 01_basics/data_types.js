fda36683ba64d4c6333c7d62e02071326e804aa2
Tanu Tamrakar <167414122+tamrakartanu04@users.noreply.github.com>
2025-05-10 07:59:20 +0000

    overview of data types

---
+++ 01_basics/data_types.js
@@ -0,0 +1,35 @@
+"use Strict";      //  treat all JS code as newer version
+
+// alert("Hello")  // here it will give error bcz we are not using browser
+
+// console.log("h5hr
+//     hrehreh");   // error
+
+console.log(`h5hr
+    hrehreh`);      // works
+
+console.log(3+   //  works but not readable
+    3
+);
+
+console.log(5);
+console.log("hii")
+console.log("true")
+
+// data types
+
+// number : range -(2^51 - 1) to (2^51 - 1)
+// bigInt
+// string = " "
+// boolean = false/true
+// null : standAlone value 
+// undefined
+// symbol : unique
+
+console.log(typeof "hii")
+console.log(typeof("Hii"))
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)      // object
+console.log(typeof Number)    // function
+console.log(typeof String)    // function
