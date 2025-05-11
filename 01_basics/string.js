6e1eb7323ca695d13961f87575c70ec8c733b2be
Tanu Tamrakar <167414122+tamrakartanu04@users.noreply.github.com>
2025-05-11 09:24:23 +0000

    string methods

---
+++ 01_basics/string.js
@@ -0,0 +1,13 @@
+let name = "tanuuuuu"
+console.log(name.toUpperCase());
+console.log(name);
+
+
+let temp = new String("helooooo")
+console.log(temp.toUpperCase())
+console.log(temp)
+
+
+let me = temp
+me = "muskan"
+console.log(temp)
