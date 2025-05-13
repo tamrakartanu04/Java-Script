b0c76c1b0a8b4abf97ef1c91c1cc387e9212b924
Tanu Tamrakar <167414122+tamrakartanu04@users.noreply.github.com>
2025-05-13 16:25:29 +0000

    Array basics

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -10,6 +10,7 @@ const arr = new Array(1, 2, 2, 4, 5)
 
 
 // array methods
+
 // arr.push(-1)
 // arr.push(0)   // adds element at end
 // console.log(arr)
@@ -23,11 +24,26 @@ const arr = new Array(1, 2, 2, 4, 5)
 // arr.shift() // removes frist element, and shift each element to one position left
 // console.log(arr)
 
-console.log(arr.includes(10)) // gives true if present, otherwise false
-console.log(arr.indexOf(2)) // gives index at which it is present, if not then return -1
+// console.log(arr.includes(10)) // gives true if present, otherwise false
+// console.log(arr.indexOf(2)) // gives index at which it is present, if not then return -1
 
 
 let newArr = arr.join()    // it converts arr into string
-console.log(newArr)
-console.log(typeof newArr) // string
+// console.log(newArr)
+// console.log(typeof newArr) // string
+// console.log(newArr[1]) // ,
+
+
+
+// slice , splice
+// splice also delete those elements from array which are taken in splice
+// means "splice manipualtes the original array"
+
+console.log("A" , arr)      // for here name will be printed A // A [1,2,2,4,5]
+
+console.log(arr.slice(1,3)) // (2, 2) // 3rd index not include
+console.log("B", arr)       // B [1, 2, 2, 4, 5]
+
+console.log(arr.splice(1,3)) // (2, 2, 3) // 3rd index included
+console.log("C" , arr)       // C [1, 5] // 
 
